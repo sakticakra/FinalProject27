5493637ef6723121f5d15147e7d5d2c49536f99c
Faishol <faisholamali746@gmail.com>
2025-09-24 13:57:07 +0700

    MENAMBAHKAN EDIT

--- Testt.java
+++ Testt.java
@@ -1,5 +1,6 @@
 public class Testt {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("EDIT BY FAISHOL");
     }
 }
